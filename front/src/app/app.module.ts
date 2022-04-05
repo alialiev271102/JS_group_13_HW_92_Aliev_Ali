55ac5d7ccacd42d6a7d7e5cc570692ee68e0b386
Limon <ali.aliev.27.11.02@gmail.com>
2022-04-05 17:33:43 +0600

    Made page-proofs

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MessengerComponent } from './pages/messenger/messenger.component';
 import { RegistarationComponent } from './pages/registaration/registaration.component';
+import { ChatComponent } from './pages/messenger/chat/chat.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     MessengerComponent,
-    RegistarationComponent
+    RegistarationComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
